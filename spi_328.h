f3b3669f09e7453358969105670e41cb9a640ec5
Alexander Nabla Becker <becker.alexander42@gmail.com>
2016-09-21 01:04:15 +0200

    removed obsolete funktion definition

--- spi_328.h
+++ spi_328.h
@@ -13,8 +13,7 @@
 #define SPI_PACKET_LENGTH 6
 #define SPI_PACKET_AMOUNT 4
 
-void init_spi_master(void);
-void init_spi_slave(void);
+void init_spi(void);
 uint8_t spi_slave_read(uint8_t data[], uint8_t start);
 void spi_slave_write(uint8_t outputdata[],uint8_t start, uint8_t size);
 uint8_t spi_master_read(uint8_t data[], uint8_t start);
